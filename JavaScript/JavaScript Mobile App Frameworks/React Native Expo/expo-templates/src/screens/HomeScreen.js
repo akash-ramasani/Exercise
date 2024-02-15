42a7856668117907a0f30fcaa67b3fc862974abb
Akash <akash.enlighten@gmail.com>
2024-02-15 15:24:10 +0530

    HomeScreen.js

--- JavaScript/JavaScript Mobile App Frameworks/React Native Expo/expo-templates/src/screens/HomeScreen.js
+++ JavaScript/JavaScript Mobile App Frameworks/React Native Expo/expo-templates/src/screens/HomeScreen.js
@@ -2,35 +2,15 @@ import React from 'react';
 
 import { StatusBar } from 'expo-status-bar';
 
-import { View, Text, useColorScheme, Appearance, StyleSheet } from 'react-native'
+import { Appearance } from 'react-native'
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
     
     let colorScheme = Appearance.getColorScheme();
-    console.log(colorScheme);
 
     return (
-        <View style={styles.container}>
-            <StatusBar style={colorScheme === 'light' ? 'dark' : 'light'} backgroundColor={colorScheme === 'light' ? 'white': 'black'}/>
-
-            <View style={{flex: 0.1, backgroundColor: 'pink'}}>
-                <Text>Expo Templates</Text>
-            </View>
-
-            <View style={{ flex: 0.9, backgroundColor: 'yellow' }}>
-                <Text>Hai</Text>
-            </View>
-        </View>
+        <StatusBar style={colorScheme === 'light' ? 'auto' : 'light'} backgroundColor={colorScheme === 'light' ? 'white': 'black'} animated={true} />
     )
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: '#fff',
-        padding: 20,
-    },
-});
