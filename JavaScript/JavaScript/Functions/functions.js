e9b96b98531e76b84096d78fd30e56eeb8782f2e
Akash <akash.enlighten@gmail.com>
2024-03-25 22:53:24 +0530

    End of Python.

---
+++ JavaScript/JavaScript/Functions/functions.js
@@ -0,0 +1,7 @@
+// A JavaScript funcrtion is defined with the function keyword, followed by a name, followed by parantheses().
+
+function greeting() {
+    console.log("Good morning, Akash.");
+}
+
+greeting();
