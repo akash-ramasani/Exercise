ee2fd6f995822073c0f81c25aecab4106d9ffe74
Akash <akash.enlighten@gmail.com>
2024-04-27 16:19:59 +0530

    Flexbox

--- JavaScript/BOM/Window.js
+++ JavaScript/BOM/Window.js
@@ -1,2 +1,8 @@
-document.getElementById("width").innerHTML = window.innerWidth;
-window.document.getElementById("height").innerHTML = window.innerHeight;
+window.document.getElementById("windowHeight").innerHTML = window.innerHeight;
+document.getElementById("windowWidth").innerHTML = window.innerWidth;
+
+document.getElementById("ScreenHeight").innerHTML = screen.height;
+document.getElementById("ScreenWidth").innerHTML = screen.width;
+
+document.getElementById("ScreenAvailHeight").innerHTML = screen.availHeight;
+document.getElementById("ScreenAvailWidth").innerHTML = screen.availWidth;
---
+++ JavaScript/arrays.js
@@ -0,0 +1,13 @@
+const name = ["Akash", "", "Ramasani"];
+console.log(name);
+const dateOfBirth = [];
+dateOfBirth[0] = 27;
+dateOfBirth[1] = "May";
+dateOfBirth[2] =  2001;
+
+
+const address = new Array("5-74, Swamy Nivas, Road Number 3", "Shanthi Nagar Colony, Hydershakote", 500091, "Telangana");
+console.log(address);
+
+name[0] = "akash";
+console.log(name);
